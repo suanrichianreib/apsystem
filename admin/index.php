7796b7a6732c2e89d54a6e798a3c7510fc49f208
Client John Subibi <alexvgamer2@gmail.com>
2024-03-05 09:45:29 +0800

    2024

--- admin/index.php
+++ admin/index.php
@@ -4,47 +4,53 @@ if (isset($_SESSION['admin'])) {
     header('location:home.php');
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <?php include 'includes/header.php'; ?>
 
-<body class="hold-transition login-page" style="background-image: url('/apsystem/images/bgc1.png'); background-size: cover;">
-    <div class="login-box">
-	<div class="login-logo" style="font-family: 'Bahnschrift Light SemiCondensed', sans-serif;">
-    <b>Admin Login</b>
-	</div>
+<body class="hold-transition login-page">
 
-        <div class="login-box-body">
-            <p class="login-box-msg">Sign in to start your session</p>
+    <div class="background-container" style="background-image: url('/apsystem/images/bgc1.png'); background-size: cover; display: flex; align-items: center; justify-content: center; min-height: 100vh;">
+    <img src="/apsystem/images/logo.png" alt="Your Image" style="width: 100%; max-width: 250px; position: absolute; top: 14%; right: 50%; transform: translate(50%, -50%);">
+            <div class="login-box max-width-600 mx-auto" style="border: 20px solid white; background-color: white; border-radius: 10px;">
+                <div class="login-logo font-family-Bahnschrift-Light-SemiCondensed font-size-20 text-align-right margin-right-70">
+                    <b>Login as an Admin user</b>
+                </div>
+                <div class="text-align-right margin-right-10">
+                    <p class="login-box-msg">Sign in to start your session</p>
+                </div>
 
-            <form action="login.php" method="POST">
-                <div class="form-group has-feedback">
-                    <input type="text" class="form-control" name="username" placeholder="input Username" required autofocus>
-                    <span class="glyphicon glyphicon-user form-control-feedback"></span>
-                </div>
-                <div class="form-group has-feedback">
-                    <input type="password" class="form-control" name="password" placeholder="input Password" required>
-                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
-                </div>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <button type="submit" class="btn btn-primary btn-block btn-flat" name="login"><i
-                                class="fa fa-sign-in"></i> Sign In</button>
-                    </div>
-                </div>
-            </form>
+                <form action="login.php" method="POST">
+    <div class="form-group">
+        <input type="text" class="form-control border-radius-10" name="username" placeholder="Input Username" required autofocus style="border-radius: 10px;">
+    </div>
+    <div class="form-group">
+        <input type="password" class="form-control border-radius-10" name="password" placeholder="Input Password" required style="border-radius: 10px;">
+    </div>
+    <div class="row">
+        <div class="col-xs-12">
+            <button type="submit" class="btn btn-primary btn-block btn-flat border-radius-10" name="login" required autofocus style="border-radius: 10px;">
+                <i class="fa fa-sign-in"></i> Sign In
+            </button>
         </div>
-        <?php
-        if (isset($_SESSION['error'])) {
-            echo "
-                <div class='callout callout-danger text-center mt20'>
-                    <p>" . $_SESSION['error'] . "</p> 
-                </div>
-            ";
-            unset($_SESSION['error']);
-        }
-        ?>
+    </div>
+</form>
+
+
+                <?php
+                if (isset($_SESSION['error'])) {
+                    echo "
+                        <div class='callout callout-danger text-center mt-2'>
+                            <p>" . $_SESSION['error'] . "</p> 
+                        </div>
+                    ";
+                    unset($_SESSION['error']);
+                }
+                ?>
+            </div>
+        </div>
     </div>
 
     <?php include 'includes/scripts.php' ?>
