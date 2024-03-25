4a665ed90bdc14018a9c7e7983549c80a136c85e
Richian <Richian>
2024-03-25 08:49:47 +0800

    image profile viewbale

--- admin/includes/menubar.php
+++ admin/includes/menubar.php
@@ -3,14 +3,18 @@
     <section class="sidebar">
       <!-- Sidebar user panel -->
       <div class="user-panel">
-        <div class="pull-left image">
-          <img src="<?php echo (!empty($user['photo'])) ? '../images/'.$user['photo'] : '../images/profile.jpg'; ?>" class="img-circle" alt="User Image">
-        </div>
-        <div class="pull-left info">
-          <p><?php echo $user['firstname'].' '.$user['lastname']; ?></p>
-          <a><i class="fa fa-circle text-success"></i> Online</a>
-        </div>
-      </div>
+  <div class="pull-left">
+    <a href="../images/<?php echo (!empty($user['photo'])) ? $user['photo'] : 'profile.jpg'; ?>" target="_blank">
+      <img src="../images/<?php echo (!empty($user['photo'])) ? $user['photo'] : 'profile.jpg'; ?>" class="img-circle" alt="User Image" style="width: 50px; height: 50px;">
+    </a>
+  </div>
+  <div class="pull-left info">
+    <p><?php echo $user['firstname'].' '.$user['lastname']; ?></p>
+    <a><i class="fa fa-circle text-success"></i> Online</a>
+  </div>
+  <div class="clearfix"></div>
+</div>
+
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu" data-widget="tree">
         <li class="header">REPORTS</li>
